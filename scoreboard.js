7162ef93704bfc14033c3400748913eac3165b6d
bellac429 <isabella-conrad@uiowa.edu>
2024-09-08 18:41:29 -0500

    Update scoreboard.js

--- scoreboard.js
+++ scoreboard.js
@@ -4,37 +4,37 @@ let guestScore = 0
 let homeScoreDisplay = document.getElementById("home-score-el")
 let guestScoreDisplay= document.getElementById("guest-score-el")
 
-function addOneHome() {
+function addOneHome() { // add a point to home
   homeScore += 1
   homeScoreDisplay.textContent = homeScore
 }
 
-function addTwoHome() {
+function addTwoHome() { // add two points to home
   homeScore += 2
   homeScoreDisplay.textContent = homeScore
 }
 
-function addThreeHome() {
+function addThreeHome() { // add three points to home
   homeScore += 3
   homeScoreDisplay.textContent = homeScore
 }
 
-function addOneGuest() {
+function addOneGuest() { // add one point to guest
   guestScore += 1
   guestScoreDisplay.textContent = guestScore
 }
 
-function addTwoGuest() {
+function addTwoGuest() { // add two points to guest
   guestScore += 2
   guestScoreDisplay.textContent = guestScore
 }
 
-function addThreeGuest() {
+function addThreeGuest() { // add three points to guest
   guestScore += 3
   guestScoreDisplay.textContent = guestScore
 }
 
-function newGame() {
+function newGame() { // reset game an scores to zero
   homeScore = 0
   guestScore = 0
   homeScoreDisplay.textContent = homeScore
